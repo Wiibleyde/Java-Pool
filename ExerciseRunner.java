31217a383afbceea49931a5c184116c692a06567
Wiibleyde <nathan@bonnell.fr>
2023-02-06 10:42:11 +0100

    computearry
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- ExerciseRunner.java
+++ ExerciseRunner.java
@@ -1,7 +1,8 @@
 public class ExerciseRunner {
     public static void main(String[] args) {
-        System.out.println(Palindrome.isPalindrome("resSasser"));
-        System.out.println(Palindrome.isPalindrome("Bonjour"));
-        System.out.println(Palindrome.isPalindrome("Hello"));
+        int[] array = ComputeArray.computeArray(new int[]{9, 13, 8, 23, 1, 0, 89});
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
     }
 }
