02d378b5c45c2df525c42240a7ada1facc50ea22
Wiibleyde <nathan@bonnell.fr>
2023-02-06 10:55:43 +0100

    iseven opti
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- IsEven.java
+++ IsEven.java
@@ -1,9 +1,5 @@
 public class IsEven {
     public static boolean isEven(int a) {
-        if ((a % 2) == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return a%2 == 0 ? false : true;
     }
 }
