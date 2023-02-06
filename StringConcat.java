d6b8c63ba831497bd73fbfe340f3dba103f762b3
Wiibleyde <nathan@bonnell.fr>
2023-02-06 10:11:02 +0100

    string concat
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- StringConcat.java
+++ StringConcat.java
@@ -1,5 +1,10 @@
+import java.lang.annotation.Retention;
+
 public class StringConcat {
     public static String concat(String s1, String s2) {
+        if ((s1 == null) || (s2 == null)) {
+            return null;
+        }
         return s1+s2;
     }
 }
