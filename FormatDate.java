39350f762cf5d4bc6e093a61c93185d9e6bf2dde
Wiibleyde <nathan@bonnell.fr>
2023-02-06 14:28:33 +0100

    format date

--- FormatDate.java
+++ FormatDate.java
@@ -6,15 +6,15 @@ import java.util.Locale;
 public class FormatDate {
 
     public static String formatToFullText(LocalDateTime dateTime) {
-        return dateTime.format(java.time.format.DateTimeFormatter.ofPattern("'Le' dd MMMM 'de' 'l'\'an 'yyyy' à 'HH'h'mm'm et' ss's'"));
+        return dateTime == null ? null : dateTime.format(java.time.format.DateTimeFormatter.ofPattern("'Le' dd MMMM 'de' 'l'\'an 'yyyy' à 'HH'h'mm'm et' ss's'"));
     }
 
     public static String formatSimple(LocalDate date) {
-        return date.format(java.time.format.DateTimeFormatter.ofPattern("MMMM dd yy", Locale.ITALIAN));
+        return date == null ? null : date.format(java.time.format.DateTimeFormatter.ofPattern("MMMM dd yy", Locale.ITALIAN));
     }
 
     public static String formatIso(LocalTime time) {
-        return time.format(java.time.format.DateTimeFormatter.ofPattern("HH:mm:ss.SSSSSSSSS"));
+        return time == null ? null : time.format(java.time.format.DateTimeFormatter.ofPattern("HH:mm:ss.SSSSSSSSS"));
     }
 
 }
