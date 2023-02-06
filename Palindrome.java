6c478427cdcf87b44aa6f3a81fac714189308843
Wiibleyde <nathan@bonnell.fr>
2023-02-06 10:33:36 +0100

    fix palind
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- Palindrome.java
+++ Palindrome.java
@@ -1,5 +1,8 @@
 public class Palindrome {
     public static boolean isPalindrome(String s) {
+        if (s == null) {
+            return false;
+        }
         int n = s.length();
         s = s.toLowerCase();
         for (int i = 0; i < (n/2); ++i) {
