69d7bdc7b92b9a1ab7354c809594544a44fc91dd
Wiibleyde <nathan@bonnell.fr>
2023-02-06 10:15:29 +0100

    str lenght

--- StringLength.java
+++ StringLength.java
@@ -1,8 +1,5 @@
 public class StringLength {
     public static int getStringLength(String s) {
-        if (s == null) {
-            return 0;
-        }
-        return s.length();
+        return s == null ? 0 : s.length();
     }
 }
