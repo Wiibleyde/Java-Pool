18ae6cb34967aeeadfcc22eb0b96d80ca85e4536
Wiibleyde <nathan@bonnell.fr>
2023-02-06 10:22:27 +0100

    ispalindrome
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- StringConcat.java
+++ StringConcat.java
@@ -1,12 +1,7 @@
-import java.lang.annotation.Retention;
-
 public class StringConcat {
     public static String concat(String s1, String s2) {
-        if (s1 == null) {
-            return s2;
-        } else if (s2 == null) {
-            return s1;
-        } else 
-        return s1+s2;
+        if (s1 == null) return s2;
+        else if (s2 == null) return s1;
+        else return s1+s2;
     }
 }
