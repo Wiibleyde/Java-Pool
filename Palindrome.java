b6bb08d09421f2cdcec498c34c52bc031d7dbe83
Wiibleyde <nathan@bonnell.fr>
2023-02-06 10:26:58 +0100

    palindrome
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- Palindrome.java
+++ Palindrome.java
@@ -2,11 +2,12 @@ public class Palindrome {
     public static boolean isPalindrome(String s) {
         int n = s.length();
         for (int i = 0; i < (n/2); ++i) {
-           if (s.charAt(i) != s.charAt(n - i - 1)) {
+            Character curString = s.charAt(i);
+            Character lasCharacter = s.charAt(n - i - 1);
+            if (curString.toUpperCase(0) != lasCharacter.toUpperCase(0)) {
                return false;
-           }
+            }
         }
-      
         return true;
     }
 }
