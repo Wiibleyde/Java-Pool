2b2102ac836295c1c98cb1f0c23be18ee2452617
Wiibleyde <nathan@bonnell.fr>
2023-02-06 14:34:41 +0100

    fix month date formatter
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- FormatDate.java
+++ FormatDate.java
@@ -6,7 +6,8 @@ import java.util.Locale;
 public class FormatDate {
 
     public static String formatToFullText(LocalDateTime dateTime) {
-        return dateTime == null ? null : dateTime.format(java.time.format.DateTimeFormatter.ofPattern("'Le' dd MMMM 'de' 'l'\'an 'yyyy' à 'HH'h'mm'm et' ss's'", Locale.FRANCE));
+        if (dateTime == null) return null;
+        return dateTime.format(java.time.format.DateTimeFormatter.ofPattern("'Le' dd MMM 'de' 'l'\'an 'yyyy' à 'HH'h'mm'm et' ss's'", Locale.FRANCE));
     }
 
     public static String formatSimple(LocalDate date) {
