9affec663571dd846311d7707d4ec24fb1459094
Wiibleyde <nathan@bonnell.fr>
2023-02-06 10:32:37 +0100

    palin
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- ExerciseRunner.java
+++ ExerciseRunner.java
@@ -1,6 +1,7 @@
 public class ExerciseRunner {
     public static void main(String[] args) {
-        System.out.println(Palindrome.isPalindrome("ressasser"));
+        System.out.println(Palindrome.isPalindrome("resSasser"));
         System.out.println(Palindrome.isPalindrome("Bonjour"));
+        System.out.println(Palindrome.isPalindrome("Hello"));
     }
 }
--- Palindrome.java
+++ Palindrome.java
@@ -1,11 +1,10 @@
 public class Palindrome {
     public static boolean isPalindrome(String s) {
         int n = s.length();
+        s = s.toLowerCase();
         for (int i = 0; i < (n/2); ++i) {
-            Character curString = s.charAt(i);
-            Character lasCharacter = s.charAt(n - i - 1);
-            if (curString.toUpperCase(0) != lasCharacter.toUpperCase(0)) {
-               return false;
+            if (s.charAt(i) != s.charAt(n - i - 1)) {
+                return false;
             }
         }
         return true;
