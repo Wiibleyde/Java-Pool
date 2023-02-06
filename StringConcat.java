b08ad5b1305f9ee471740c06418db2656d6f7c04
Wiibleyde <nathan@bonnell.fr>
2023-02-06 10:13:53 +0100

    String concat fix
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- StringConcat.java
+++ StringConcat.java
@@ -2,9 +2,11 @@ import java.lang.annotation.Retention;
 
 public class StringConcat {
     public static String concat(String s1, String s2) {
-        if ((s1 == null) || (s2 == null)) {
-            return null;
-        }
+        if (s1 == null) {
+            return s2;
+        } else if (s2 == null) {
+            return s1;
+        } else 
         return s1+s2;
     }
 }
