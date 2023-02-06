d76b9de63a0e6d96298eac0f2f3322f6345143e3
Wiibleyde <nathan@bonnell.fr>
2023-02-06 11:42:38 +0100

    list search update
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- ListSearchIndex.java
+++ ListSearchIndex.java
@@ -4,12 +4,18 @@ import java.util.List;
 
 public class ListSearchIndex {
     public static Integer findLastIndex(List<Integer> list, Integer value) {
+        if (list == null) return null;
+        if (value == null) return null;
         return list.lastIndexOf(value) == -1 ? null : list.lastIndexOf(value);
     }
     public static Integer findFirstIndex(List<Integer> list, Integer value) {
+        if (list == null) return null;
+        if (value == null) return null;
         return list.indexOf(value) == -1 ? null : list.indexOf(value);
     }
     public static List<Integer> findAllIndexes(List<Integer> list, Integer value) {
+        if (list == null) return null;
+        if (value == null) return null;
         List<Integer> indexes = new ArrayList<>();
         for (int i = 0; i < list.size(); ++i) {
             if (list.get(i).equals(value)) {
