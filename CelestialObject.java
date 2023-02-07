647f2652a633472c0d163f347d136d3f1c1c76ab
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2023-02-07 16:05:11 +0100

    Object
    Signed-off-by: Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>

--- CelestialObject.java
+++ CelestialObject.java
@@ -3,4 +3,19 @@ public class CelestialObject {
     public double y;
     public double z;
     public String name;
+    // two constructors : The default one, with no parameter 
+
+    public CelestialObject() {
+        this.x = 0.0;
+        this.y = 0.0;
+        this.z = 0.0;
+        this.name = "Soleil";
+    }
+    
+    public CelestialObject(String name, double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+    }
 }
--- ExerciseRunner.java
+++ ExerciseRunner.java
@@ -1,9 +1,16 @@
-import java.util.List;
-import java.util.Map;
-
 public class ExerciseRunner {
 
     public static void main(String[] args) {
-        System.out.println(WeddingComplex.createBestCouple(Map.of("Naruto", List.of("Sakura", "Hinata"), "Sasuke", List.of("Sakura", "Hinata")),Map.of("Sakura", List.of("Sasuke", "Naruto"), "Hinata", List.of("Naruto", "Sasuke"))));
+        CelestialObject defaultStar = new CelestialObject();
+        System.out.println(defaultStar.x);
+        System.out.println(defaultStar.y);
+        System.out.println(defaultStar.z);
+        System.out.println(defaultStar.name);
+
+        CelestialObject earth = new CelestialObject("Terre", 0.43, 0.98, 1.43);
+        System.out.println(earth.x);
+        System.out.println(earth.y);
+        System.out.println(earth.z);
+        System.out.println(earth.name);
     }
 }
