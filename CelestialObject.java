af4408ec09833be113826657bc301675b779da86
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2023-02-07 16:01:17 +0100

    star
    Signed-off-by: Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>

---
+++ CelestialObject.java
@@ -0,0 +1,13 @@
+public class CelestialObject {
+    // x (double) y (double) z (double) name (String)
+    private double x;
+    private double y;
+    private double z;
+    private String name;
+    public CelestialObject(double x, double y, double z, String name) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+    }
+}
