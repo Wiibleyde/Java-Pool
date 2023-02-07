930ded1150565d8546664bbd23802fbe10f3aba6
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2023-02-07 16:07:00 +0100

    getter and setter
    Signed-off-by: Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>

--- CelestialObject.java
+++ CelestialObject.java
@@ -18,4 +18,36 @@ public class CelestialObject {
         this.z = z;
         this.name = name;
     }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public double getZ() {
+        return this.z;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- ExerciseRunner.java
+++ ExerciseRunner.java
@@ -2,15 +2,18 @@ public class ExerciseRunner {
 
     public static void main(String[] args) {
         CelestialObject defaultStar = new CelestialObject();
-        System.out.println(defaultStar.x);
-        System.out.println(defaultStar.y);
-        System.out.println(defaultStar.z);
-        System.out.println(defaultStar.name);
+        System.out.println(defaultStar.getX());
+        System.out.println(defaultStar.getY());
+        System.out.println(defaultStar.getZ());
+        System.out.println(defaultStar.getName());
 
-        CelestialObject earth = new CelestialObject("Terre", 0.43, 0.98, 1.43);
-        System.out.println(earth.x);
-        System.out.println(earth.y);
-        System.out.println(earth.z);
-        System.out.println(earth.name);
+        defaultStar.setName("Terre");
+        defaultStar.setX(0.43);
+        defaultStar.setY(0.98);
+        defaultStar.setZ(1.43);
+        System.out.println(defaultStar.getX());
+        System.out.println(defaultStar.getY());
+        System.out.println(defaultStar.getZ());
+        System.out.println(defaultStar.getName());
     }
 }
