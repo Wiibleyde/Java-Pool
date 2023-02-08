8e9c284e901037012ef92a6fb00ae1e96a81c02f
Wiibleyde <nathan@bonnell.fr>
2023-02-08 17:17:14 +0100

    pdate
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- Galaxy.java
+++ Galaxy.java
@@ -35,28 +35,25 @@ public class Galaxy {
         this.celestialObjects = celestialObjects;
     }
 
-    public Map<String, String> computeMassRepartition() {
-        Map<String, Double> massRepartition = new HashMap<>();
-        massRepartition.put("Other", 0.0);
-        massRepartition.put("Planet", 0.0);
-        massRepartition.put("Star", 0.0);
+    public Map<String, Integer> computeMassRepartition() {
+        Map<String, Integer> massRepartition = new HashMap<>(Map.of("Other", 0, "Planet", 0, "Star", 0));
     
         for (CelestialObject celestialObject : this.celestialObjects) {
-            if (celestialObject instanceof Star) {
-                massRepartition.put("Star", massRepartition.get("Star") + celestialObject.getMass());
-            } else if (celestialObject instanceof Planet) {
+            if (celestialObject instanceof Planet) {
                 massRepartition.put("Planet", massRepartition.get("Planet") + celestialObject.getMass());
+            } else if (celestialObject instanceof Star) {
+                massRepartition.put("Star", massRepartition.get("Star") + celestialObject.getMass());
             } else {
                 massRepartition.put("Other", massRepartition.get("Other") + celestialObject.getMass());
             }
         }
     
-        Map<String, String> formattedMassRepartition = new HashMap<>();
-        for (Map.Entry<String, Double> entry : massRepartition.entrySet()) {
-            double value = entry.getValue();
-            formattedMassRepartition.put(entry.getKey(), value == (int) value ? Integer.toString((int) value) : Double.toString(value));
-        }
-        return formattedMassRepartition;
+        // Map<String, String> formattedMassRepartition = new HashMap<>();
+        // for (Map.Entry<String, Double> entry : massRepartition.entrySet()) {
+        //     double value = entry.getValue();
+        //     formattedMassRepartition.put(entry.getKey(), value == (int) value ? Integer.toString((int) value) : Double.toString(value));
+        // }
+        return massRepartition;
     }
         
 }
