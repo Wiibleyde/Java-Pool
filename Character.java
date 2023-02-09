9318915d26a2a3b3efde968363a135ef4e44dd15
Wiibleyde <nathan@bonnell.fr>
2023-02-09 10:33:01 +0100

    fight status
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- Character.java
+++ Character.java
@@ -1,12 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Character {
     private final int maxHealth;
     private int currentHealth;
     private final String name;
+    private static List<Character> allCharacters = new ArrayList<Character>();
 
     public Character(String name, int maxHealth) {
         this.name = name;
         this.maxHealth = maxHealth;
         this.currentHealth = maxHealth;
+        addCharacter(this);
     }
 
     public int getMaxHealth() {
@@ -38,5 +43,37 @@ public class Character {
 
     public void attack(Character character) {
         character.takeDamage(9);
+    }
+
+    public void addCharacter(Character character) {
+        allCharacters.add(character);
+    }
+
+    public static String printStatus() {
+        String line = "------------------------------------------";
+        String title = "Characters currently fighting : ";
+        String characters = "";
+        if (allCharacters.isEmpty()) {
+            return line + "\nNobody's fighting right now !\n" + line;
+        }
+        for (Character character : allCharacters) {
+            characters += "- " + character.toString() + "\n";
+        }
+        return line + "\n" + title + "\n" + characters + line;
+    }
+
+    public static Character fight(Character character1, Character character2) {
+        while (character1.getCurrentHealth() > 0 && character2.getCurrentHealth() > 0) {
+            character1.attack(character2);
+            if (character2.getCurrentHealth() > 0) {
+                character2.attack(character1);
+            }
+        }
+        if (character1.getCurrentHealth() == 0) {
+            allCharacters.remove(character1);
+            return character2;
+        }
+        allCharacters.remove(character2);
+        return character1;
     }
 }
