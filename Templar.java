aa54466a1cb7a3b3b0bb17c070700ccbc2c17a9f
Wiibleyde <nathan@bonnell.fr>
2023-02-09 15:05:54 +0100

    templar
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

---
+++ Templar.java
@@ -0,0 +1,36 @@
+public class Templar extends Character implements Healer, Tank {
+    private final int healCapacity;
+    private final int shield;
+
+    public Templar(String name, int maxHealth, int healCapacity, int shield) {
+        super(name, maxHealth);
+        this.healCapacity = healCapacity;
+        this.shield = shield;
+    }
+
+    public int getHealCapacity() {
+        return healCapacity;
+    }
+
+    @Override
+    public void heal(Character character) {
+        int currentHealth = character.getCurrentHealth();
+        int newHealth = currentHealth + this.healCapacity;
+        if (newHealth > character.getMaxHealth()) {
+            newHealth = character.getMaxHealth();
+        }
+        character.currentHealth = newHealth;
+    }
+
+    public int getShield() {
+        return shield;
+    }
+
+    @Override
+    public String toString() {
+        if (this.currentHealth == 0) {
+            return this.getName() + " has been beaten, even with its " + this.shield + " shield. So bad, it could heal " + this.healCapacity + " HP.";
+        }
+        return this.getName() + " is a strong Templar with " + this.currentHealth + " HP. It can heal " + this.healCapacity + " HP and has a shield of " + this.shield + " HP.";
+    }
+}
