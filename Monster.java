e58642bed1613406b9975037efcb616dafd3d761
Wiibleyde <nathan@bonnell.fr>
2023-02-09 11:42:59 +0100

    ds
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- Monster.java
+++ Monster.java
@@ -6,7 +6,7 @@ public class Monster extends Character {
     @Override
     public String toString() {
         if (this.getCurrentHealth() == 0) {
-            return this.getName() + " is a monster and is dead";
+            return this.getName() + " is a monster and is dead.";
         }
         return this.getName() + " is a monster with " + this.getMaxHealth() + " HP.";
     }
