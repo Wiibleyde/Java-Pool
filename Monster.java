b82b3a91c4bd976bf285c5ef34eaabc63e5d1fd4
Wiibleyde <nathan@bonnell.fr>
2023-02-09 11:56:37 +0100

    not costa
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- Monster.java
+++ Monster.java
@@ -3,19 +3,11 @@ public class Monster extends Character {
         super(name, maxHealth);
     }
     
-    // @Override
-    // public String toString() {
-    //     if (this.getCurrentHealth() == 0) {
-    //         return this.getName() + " is a monster and is dead";
-    //     }
-    //     return this.getName() + " is a monster with " + this.getMaxHealth() + " HP";
-    // }
-
     @Override
-    public String toString(){
-        if(this.getCurrentHealth() != 0){
-            return String.format("%s is a monster with %d HP.",this.getName(),this.getCurrentHealth()) ;
+    public String toString() {
+        if (this.getCurrentHealth() == 0) {
+            return this.getName() + " is a monster and is dead";
         }
-        return String.format("%s is a monster and is dead.", this.getName());
+        return this.getName() + " is a monster with " + this.getMaxHealth() + " HP";
     }
 }
